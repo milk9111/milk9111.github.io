72809e494900d27138792ea9fc6168fff979b646
milk9111 <connorlndbrg349@gmail.com>
2018-01-26 09:10:24 -0800

    TCSS 491 Assignment 1 submission.

---
+++ js/projectile.js
@@ -0,0 +1,84 @@
+
+
+/**
+ * This is the 'constructor' for the Projectile object. It holds all of the instance fields
+ * for the Projectile like animations and what the current motion/direction they are doing is.
+ *
+ * @param game The instance of the GameEngine
+ * @param animation The animation for the Projectile to use when it is being drawn
+ * @constructor
+ * @author Connor Lundberg
+ */
+class Projectile extends Entity {
+
+    constructor(game, animation, direction, startX, startY) {
+
+        super(game, startX, startY, true, 64, 64, 0, 0, "projectile"); //(0, 400) signify where the sprite will be drawn.
+
+        this.game = game;
+
+        this.shootAnimation = animation;
+
+        this.facingDirection = direction;
+
+        this.maxAnimationLoopsBeforeRemoval = 5;
+    }
+
+
+    /**
+     * Here the Projectile will move forward in the direction it was shot from
+     *
+     * @author Connor Lundberg
+     */
+    update() {
+        if (this.shootAnimation.timesFinished >= this.maxAnimationLoopsBeforeRemoval) {
+            super.removal = true;
+            this.shootAnimation.timesFinished = 0;
+        }
+
+
+        let totalDistance = 2;
+        let distance = 0;
+
+        switch (this.facingDirection) {
+            case 1: //forward
+                distance = totalDistance;
+                this.y = this.y - distance;
+                break;
+            case 2: //downward
+                distance = totalDistance;
+                this.y = this.y + distance;
+                break;
+            case 3: //left
+                distance = totalDistance;
+                this.x = this.x - distance;
+                break;
+            case 4: //right
+                distance = totalDistance;
+                this.x = this.x + distance;
+                break;
+            default: //anything else
+                alert("Incorrect direction for projectile to move");
+                return;
+        }
+
+        Entity.prototype.update.call(this);
+
+    }
+
+
+    /**
+     * Here the animation for the projectile will be called.
+     *
+     * @param ctx The context to draw the image onto.
+     * @author Connor Lundberg
+     */
+    draw(ctx) {
+
+        this.shootAnimation.drawFrame(this.game, this.game.clockTick, ctx, this.x, this.y);
+        Entity.prototype.draw.call(this);
+    }
+
+}
+
+
